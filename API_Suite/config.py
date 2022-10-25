41932bdb6d870382faebdf0dc750f0dab3b137c4
balaji-githubstore <balaji0017@gmail.com>
2022-10-25 20:14:26 +0530

    api automation

---
+++ API_Suite/config.py
@@ -0,0 +1,11 @@
+BASE_URI='http://localhost:8080'
+RESOURCE_API_TOKEN="/api/auth/token"
+RESOURCE_GET_ALL_USERS="/api/users"
+RESOURCE_POST_USER="/api/users"
+RESOURCE_GET_USERS="/api/users/kim_0198d"
+RESOURCE_PUT_USERS="/api/users/kim_0198d"
+
+JSON_ADD_USER="../test_data/add_users.json"
+
+
+
